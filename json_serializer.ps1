8356e241bc8d307a5256624d20da382bd565fb87
Or Ohev Zion <or.ohev.zion@gmail.com>
2018-12-17 11:00:17 +0200

    Convert PSObject to Hashtable datatype to support more functionality on deserialized data.
    Also add support for unicode chars.

--- json_serializer.ps1
+++ json_serializer.ps1
@@ -34,6 +34,7 @@ Function ConvertTo-JSON-Stable {
                 "String" {
                     if ($value -ne "null")
                     {
+			            $value = $value.Replace('"', '@ESCAPED_QUOTE_PLACEHOLDER@')
                         $value = "`"$value`""
                     }
                 }
@@ -42,13 +43,14 @@ Function ConvertTo-JSON-Stable {
             $data  = ""
             if ($keyValue)
             {
+                $name = "$name".Replace('"', '@ESCAPED_QUOTE_PLACEHOLDER@')
                 $data = "`"$name`": $value"
             }
             else
             {
-                $data = "$value"
+                $data = "$value"  # .Replace('"', '\"')
             }
-            $data = $data.Replace("\", "\\")
+            $data = $data.Replace("\", "\\").Replace('@ESCAPED_QUOTE_PLACEHOLDER@', '\"')
             $data
         }
 
@@ -195,284 +197,183 @@ Function ConvertFrom-JSON-Stable {
         $json,
         [switch]$raw
     )
-    # TODO: Replace logic of convertsion from json object to powershell object. Currently we create a PSCustomObject with following syntax:
-    # PsObject syntax: ** New-Object PSObject | Add-Member -Passthru NoteProperty "key1" "value2" | Add-Member -Passthru NoteProperty "key2" "value2" **
-    # We should try to create a Hashtable, which is neat and simpler than PSCustomObject, with the following *possible* syntax:
-    # Hashtable possible syntax: ** @{"key1"="value1";"key2"="value2"} **
 
     Begin
     {
-        $script:stringState = $false
-        $script:appendingToUnicode = $false
-        $script:unicodeStringCandidate = ''
-        $script:stateArray=New-Object System.Collections.ArrayList
-        $script:valueState = $false
-        $global:result=""
+		$script:stateArray = New-Object System.Collections.ArrayList
+    	$script:valueState = $false
+        $global:result = ""
 
-        function scan-characters ($c) {
-			$script:out = ''
-            switch -regex ($c)
-            {
-                "{" {
-					if ($script:appendingToUnicode)
-					{
-						$script:appendingToUnicode = $false
-						if ($script:stringState)
-						{
-							$script:out += $script:unicodeStringCandidate
-						}
-						else
-						{
-							$script:unicodeStringCandidate
-						}
-						$script:unicodeStringCandidate = ''
-					}
-                    if ($script:stringState)
-                    {
-                        $script:out += $c
-                    }
-                    else
-                    {
-                        $script:out += "(New-Object PSObject "
-                        [void]$script:stateArray.Add("d")
-                        $script:valueState=$script:stringState=$false
-                    }
+    	function scan-characters ($c) {
+    		switch -regex ($c)
+    		{
+    			"{" {
+                    "(New-Object PSObject "
+					[void]$script:stateArray.Add("d")
+					$script:valueState=$false
                 }
-                "}" {
-					if ($script:appendingToUnicode)
-					{
-						$script:appendingToUnicode = $false
-						if ($script:stringState)
-						{
-							$script:out += $script:unicodeStringCandidate
-						}
-						else
-						{
-							$script:unicodeStringCandidate
-						}
-						$script:unicodeStringCandidate = ''
-					}
-                    if ($script:stringState)
-                    {
-                        $script:out += $c
-                    }
-                    else
-                    {
-                        $script:out += ")"
-                        $script:stateArray.RemoveAt($script:stateArray.Count-1)
-                    }
+    			"}" {
+                    ")"
+					$script:stateArray.RemoveAt($script:stateArray.Count-1)
                 }
                 "\[" {
-					if ($script:appendingToUnicode)
-					{
-						$script:appendingToUnicode = $false
-						if ($script:stringState)
-						{
-							$script:out += $script:unicodeStringCandidate
-						}
-						else
-						{
-							$script:unicodeStringCandidate
-						}
-						$script:unicodeStringCandidate = ''
-					}
-                    if ($script:stringState)
-                    {
-                        $script:out += $c
-                    }
-                    else
-                    {
-                        [void]$script:stateArray.Add("a")
-                        $script:out += "@("
-                    }
+                    [void]$script:stateArray.Add("a")
+					"@("
                 }
-                "\]" {
-					if ($script:appendingToUnicode)
-					{
-						$script:appendingToUnicode = $false
-						if ($script:stringState)
-						{
-							$script:out += $script:unicodeStringCandidate
-						}
-						else
-						{
-							$script:unicodeStringCandidate
-						}
-						$script:unicodeStringCandidate = ''
-					}
-                    if ($script:stringState)
-                    {
-                        $script:out += $c
-                    }
-                    else
-                    {
-                        $script:stateArray.RemoveAt($script:stateArray.Count-1)
-                        $script:out += ")"
-                    }
+    			"\]" {
+                    $script:stateArray.RemoveAt($script:stateArray.Count-1)
+					")"
                 }
                 "," {
-					if ($script:appendingToUnicode)
-					{
-						$script:appendingToUnicode = $false
-						if ($script:stringState)
-						{
-							$script:out += $script:unicodeStringCandidate
-						}
-						else
-						{
-							$script:unicodeStringCandidate
-						}
-						$script:unicodeStringCandidate = ''
+                    if($script:stateArray[$script:stateArray.Count-1] -eq "a") { "," }
+					else {
+						$script:valueState = $false
 					}
-                    if ($script:stringState)
-                    {
-                        $script:out += $c
+    			}
+    			'"' {
+					if($script:stringState -eq $true -and $script:valueState -eq $false -and $script:stateArray[$script:stateArray.Count-1] -eq "d") {
+                        ' | Add-Member -Passthru NoteProperty "'
                     }
-                    else
-                    {
-                        if($script:stateArray[$script:stateArray.Count-1] -eq "a") { $script:out += "," }
-                        else {
-                            $script:valueState = $false
-                            #$script:stringState = $false
-                        }
-                    }
+                    else { '"' }
+    			}
+
+    			":" {
+                    " "
+					$script:valueState = $true
                 }
-                '"' {
-					if ($script:appendingToUnicode)
+
+                "[\t\r\n]" {}
+
+                default {$c}
+    		}
+    	}
+
+    	function parse($target)
+    	{
+            $result = ""
+			$script:lastChar = $null
+			$script:unicodeString = ''
+			$script:stringState = $false
+            $script:escapedQuote = $false
+			$script:backslashCount = 0
+			$arr = $target.ToCharArray()
+            #Write-Host "arr: $arr"
+    		ForEach($c in $arr) {
+                #Write-Host "char: $c"
+				if ($lastChar -eq '\')
+				{
+					$script:backslashCount += 1
+				} else {
+					$script:backslashCount = 0
+				}
+				if ($c -eq '"')
+				{
+					# If we are not in stringState, we enter it now.
+					# If we are in stringState, we make sure this quote is not escaped by backslash.
+					# If it is escaped, we enter stringState again.
+					$script:stringState = !$script:stringState
+					#Write-Host "String State: $script:stringState"
+					# Now see if this is escaped quote or end of string quote.
+					if ($script:backslashCount -and $script:backslashCount % 2 -eq 1)
 					{
-						$script:appendingToUnicode = $false
-						if ($script:stringState)
-						{
-							$script:out += $script:unicodeStringCandidate
-						}
-						else
-						{
-							$script:unicodeStringCandidate
-						}
-						$script:unicodeStringCandidate = ''
+						$script:stringState = !$script:stringState
+                        $script:escapedQuote = $true
+						#Write-Host "Change String State due to escaped quote: $script:stringState"
 					}
-                    if($script:stringState -eq $false -and $script:valueState -eq $false -and $script:stateArray[$script:stateArray.Count-1] -eq "d") {
-                        #' | Add-Member -Passthru NoteProperty $([regex]::Unescape("'
-                        $script:out += ' | Add-Member -Passthru NoteProperty "'
-                    }
-                    else {
-                        #Write-Host "script:stringState: $script:stringState"
-                        #Write-Host "script:valueState: $script:valueState"
-                        #Write-Host "script:stateArray: $script:stateArray"
-                        if ($script:valueState -eq $false)
+				}
+				if ($script:stringState)
+				{
+                    if ($c -eq '"')
+                    {
+                        if (!$script:escapedQuote)
                         {
-                            #'"))'
-                            $script:out += '"'
+                            $result += scan-characters $c
                         }
                         else
                         {
-                            if (!$script:stringState)
-                            {
-                                #'$([regex]::Unescape("'
-                                $script:out += '"'
-                            }
-                            else
-                            {
-                                #'"))'
-                                $script:out += '"'
-                            }
+                            $script:unicodeString = $script:unicodeString -replace ".$", '`"'
+                            #$script:unicodeString += '@ESCAPED_QUOTE_PLACEHOLDER@'  # $c
+                            $script:escapedQuote = $false
                         }
                     }
-                    $script:stringState = !$script:stringState
-                }
-
-                ":" {
-					if ($script:appendingToUnicode)
-					{
-						$script:appendingToUnicode = $false
-						if ($script:stringState)
-						{
-							$script:out += $script:unicodeStringCandidate
-						}
-						else
-						{
-							$script:unicodeStringCandidate
-						}
-						$script:unicodeStringCandidate = ''
-					}
-                    if($script:stringState){
-                        $script:out += ":"
-                    } else {
-                        $script:out += " "
-                        $script:valueState = $true
+                    else
+                    {
+                        $script:unicodeString += $c
                     }
-                }
-
-                "[\t\r\n]" {
-					if ($script:appendingToUnicode)
+				}
+				else
+				{
+					#Write-Host "non-string char: $c"
+					if ($script:unicodeString)
 					{
-						$script:appendingToUnicode = $false
-						if ($script:stringState)
+                        $convertedString = convertUnicodeChars
+                        #Write-Host "convertedString: $convertedString"
+						$result += $convertedString
+					}
+					$result += scan-characters $c
+				}
+				$script:lastChar = $c
+    		}
+
+    		$result
+    	}
+
+		function convertUnicodeChars {
+			$newString = ''
+			$unicodeChar = ''
+			$appendingChars = $false
+			foreach ($char in $script:unicodeString.toCharArray()){
+				if ($char -eq '\')
+				{
+					if ($unicodeChar)
+					{
+						$newString += $char
+					}
+					$unicodeChar = $char
+					$appendingChars = $true
+				}
+				elseif ($appendingChars)
+				{
+					$unicodeChar += $char
+					if ($unicodeChar.Length -eq 6)
+					{
+						if ($unicodeChar -cmatch "(\\u[0-9a-fA-F]{4})")
 						{
-							$script:out += $script:unicodeStringCandidate
+							try
+							{
+								$unicodeChar = [regex]::Unescape($unicodeChar)
+								#Write-Host "Converted unicodeChar: $unicodeChar"
+							}
+							catch {}
 						}
-						else
-						{
-							$script:unicodeStringCandidate
-						}
-						$script:unicodeStringCandidate = ''
+						$newString += $unicodeChar
+						$unicodeChar = ''
+						$appendingChars = $false
 					}
 				}
-
-                default {
-                    #Write-Host "c: $c"
-                    if ($c -eq '\'){
-                        if ($script:appendingToUnicode)
-                        {
-                            $script:out += $c
-                        }
-                        $script:unicodeStringCandidate = $c
-                        $script:appendingToUnicode = $true
-                    }
-                    elseif ($script:appendingToUnicode){
-                        $script:unicodeStringCandidate += $c
-                    } else {
-                        $script:out += $c
-                    }
-					if ($script:unicodeStringCandidate.Length -eq 6)
-                    {
-                        if ($script:unicodeStringCandidate.startswith("\u") -and ($script:unicodeStringCandidate.split("\").count -eq 2))
-                        {
-                            try {
-                                #Write-Host "Converting unicode candidate: $script:unicodeStringCandidate"
-                                $script:unicodeStringCandidate = [regex]::Unescape($script:unicodeStringCandidate)
-                                #Write-Host "Converted unicode to: $script:unicodeStringCandidate"
-                            }
-                            catch {
-                                #Write-Host "Failed converting candidate: $script:unicodeStringCandidate."
-                            }
-                        }
-                        $script:out += $script:unicodeStringCandidate
-                        $script:unicodeStringCandidate = ''
-                        $script:appendingToUnicode = $false
-                    }
-                }
-            }
-			if ($script:out){
-				$script:out
+				else
+				{
+					$newString += $char
+				}
 			}
-        }
+			# When we see backslash in string '\' we assume a unicode special character is coming.
+			# We aggregate string until length of 6 is achieved.
+			# We make sure candidates contain only a-f,0-9 letters.
+			# We call [regex]::Unescape with try-catch on these potential unicode strings.
+			# We add the converted unicode character to string result.
 
-        function parse($target)
-        {
-            $result = ""
-            $firstBackslash = $true
-            ForEach($c in $target.ToCharArray()) {
-                $result += scan-characters $c
-            }
-
-            $result
-        }
+			$script:unicodeString = ''
+			$newString
+		}
     }
 
     Process {
         if($_) {
-            $result += parse $_.Replace("true", '$true').Replace("false", '$false').Replace("\\", "\")
+            # We use temp var in order to replace backslashes only after parsing,
+            # such that we can differentiate between escape backslashes and regular backslashes.
+            $temp = parse $_.Replace("true", '$true').Replace("false", '$false')
+            #Write-Host "temp: $temp"
+            $result += $temp.Replace("\\", "\").Replace('@ESCAPED_QUOTE_PLACEHOLDER@', '`"')
         }
     }
 
@@ -480,11 +381,46 @@ Function ConvertFrom-JSON-Stable {
         If($json) {
             $result = parse $json
         }
+
         #Write-Host "result: $result"
-        If(-Not $raw) {
-            $result | Invoke-Expression
-        } else {
-            $result
+        $result = $result | Invoke-Expression
+		$result = ConvertPSObjectToHashtable $result
+		$result
+    }
+}
+
+function ConvertPSObjectToHashtable
+{
+    param (
+        $InputObject
+    )
+
+    process
+    {
+        if ($null -eq $InputObject) { return $null }
+
+        if ($InputObject -is [System.Collections.IEnumerable] -and $InputObject -isnot [string])
+        {
+            $collection = @(
+                foreach ($object in $InputObject) { ConvertPSObjectToHashtable $object }
+            )
+
+			,$collection
+        }
+        elseif ($InputObject.GetType().Name -eq "PSCustomObject")
+        {
+            $hash = @{}
+
+            foreach ($property in $InputObject.PSObject.Properties)
+            {
+                $hash[$property.Name] = ConvertPSObjectToHashtable $property.Value
+            }
+
+			$hash
+        }
+        else
+        {
+            $InputObject
         }
     }
 }
